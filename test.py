ee8e437edcfdebbd98013fa32a542a85d377f3c1
timchen1216 <timchengood@gmail.com>
2022-02-27 15:53:27 +0800

    2/27

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import cv2
 
-img = cv2.imread(r'C:\Users\timch\MyPython\opencv_test\12345.png')
+img = cv2.imread(r'C:\Users\timch\MyPython\2022_library_Self-propelled-car\mnist_png\validation\0\54927.png')
 print(img.shape)
 cv2.imshow('img', img)
-img_1 = cv2.resize(img, (480, 480))
-print(img_1.shape)
-cv2.imshow('img_1', img_1)
 
 cv2.waitKey(0)
