26d76cdeaf6f97632c26c392ae12a220da3b30a3
timchen1216 <timchengood@gmail.com>
2022-03-01 14:23:08 +0800

    3/1

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 
-img = cv2.imread(r'C:\Users\timch\MyPython\2022_library_Self-propelled-car\mnist_png\validation\0\54927.png')
+img = cv2.imread(
+    r'C:\Users\timch\MyPython\2022_library_Self-propelled-car\mnist_png\validation\0\54927.png')
 print(img.shape)
 cv2.imshow('img', img)
 
