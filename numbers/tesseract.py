a209b41b93db5d22187a7ae1673cb9ff67c45fa3
timchen1216 <timchengood@gmail.com>
2022-03-25 19:34:05 +0800

    3/25

--- numbers/tesseract.py
+++ numbers/tesseract.py
@@ -4,7 +4,7 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread(r'C:\Users\timch\MyPython\2022_library_Self-propelled-car\1.png')
+img = cv2.imread(r'C:\Users\timch\MyPython\2022_library_Self-propelled-car\test_picture\numbers.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow("img",img) 
 
