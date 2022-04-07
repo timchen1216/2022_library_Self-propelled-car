7aaee9ac8ab1415574805f955093adcb88bdf4ff
aaaa4234 <100689431+aaaa4234@users.noreply.github.com>
2022-04-07 15:46:57 +0800

    Add files via upload

--- label/try.py
+++ label/try.py
@@ -7,7 +7,8 @@ shutil.rmtree('./3')
 os.mkdir('./1')
 os.mkdir('./2')
 os.mkdir('./3')
-connection = mysql.connector.connect(host='192.168.255.205',
+total = []
+connection = mysql.connector.connect(host='192.168.146.205',
                                     user='library',
                                     password='123456789')
 
@@ -24,29 +25,42 @@ cursor = connection.cursor()
 # 選擇資料庫
 cursor.execute("USE `demo`;")
 
-def RetrieveBlob(ID):
+def RetrieveBlob(ID,total):
     SQLstatement2 = "SELECT * FROM detect where number = '{0}'"
     cursor.execute(SQLstatement2.format(str(ID)))
     cab = cursor.fetchone()[1]
     SQLstatement2 = "SELECT * FROM detect where number = '{0}'"
     cursor.execute(SQLstatement2.format(str(ID)))
     Myresult = cursor.fetchone()[2]
-    storefilepath = "./"+str(cab)+"/{0}.jpg".format(str(ID)) #存到此路徑下名為1的資料夾內
+    if cab == 1:
+        name = ID
+    elif cab == 2:
+        name = ID - total[0]
+    else :
+        name = ID - total[0] - total[1]
+    storefilepath = "./"+str(cab)+"/{0}.jpg".format(str(name)) #存到此路徑下名為1的資料夾內
     with open(storefilepath,"wb") as File:
-        File.write(Myresult)
-        File.close()
-        
-        
+            File.write(Myresult)
+            File.close()
+    
+for i in range(1,4):    
+   cursor.execute("select count(*) from detect where cabinet ="+str(i)+";")
+   records = cursor.fetchall()
+   for r in records:
+      a, = r
+      total.append(a)
+      print (total)
+
 cursor.execute("select count(*) from detect;")
 records = cursor.fetchall()
 for r in records:
-    print(r)    
-a, = r
-for i in range(1,a+1):
-    RetrieveBlob(i)    
+   print(r)    
+   b, = r
+   
+for i in range(1,b+1):
+    RetrieveBlob(i,total)    
         
 
-   
 
 # 創建表格
 # cursor.execute('CREATE TABLE `qq`(qq INT);')
